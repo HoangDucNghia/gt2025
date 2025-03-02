c22be61a2a57b4cdcc7bfd49d93ce4374e138402
Hoang Duc Nghia <nghiahd.22bi13329@usth.edu.vn>
2025-03-03 00:32:21 +0700

    Update

--- TP1.(Ex1).py
+++ TP1.(Ex1).py
@@ -1,24 +1,27 @@
-def PathExistence(G,s,t):
-    u=[]
-    u.append(s)
-    a=len(G)
-    for i in range(a):
-        b=len(G[i])
-        for j in range(b):
-            c=len(u)
-            for h in range(c):
-                if G[i][j]==u[h] | G[i][j+1]==u[h]:
-                   u+=G[i][j+1]
-    for i in u:
-        if(t==i):
-            return True
-        else:
-            return False
+def PathExistence(G, s, t):
+    visited = set()
+    visited.add(s)
+    while True:
+        new_visited = set()
+        for u in visited:
+            if u in G:  
+                for v in G[u]:
+                    if v not in visited:
+                        new_visited.add(v)
+        if not new_visited:
+            break
+        visited.update(new_visited)
+    
+    if t in visited:
+        return "Yes"
+    else:
+        return "No"
 
 def main():
-    G=[[1,2],[2,5],[3,6],[6,4],[6,7],[4,7]]
-    s=int(input("Enter the first vertex"))
-    t=int(input("Enter the second vertex"))
-    result=PathExistence(G,s,t)
+    G = {1: [2], 2: [1, 5], 3: [6], 4: [6, 7], 5: [2], 6: [3, 4, 7], 7: [4, 6]}
+    s = int(input("Enter the first vertex: "))
+    t = int(input("Enter the second vertex: "))
+    result = PathExistence(G, s, t)
     print(result)
+
 main()
